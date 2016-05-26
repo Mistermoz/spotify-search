48f966f4136eb0e70d072595a9fe92b52960c20f
claudio.donoso <claudio.donoso@pc-212-168-45-190.cm.vtr.net>
2016-05-26 02:16:58 -0400

    add controller songs

--- webroot/js/controllers.js
+++ webroot/js/controllers.js
@@ -1,6 +1,70 @@
 angular.module('myApp.controllers', []);
 
 as.controller('AppCtrl', function($scope, $rootScope, $http, $location) {
-    
+    var types = '';
+    $scope.q = '';
+    $scope.checkAlbums = false, $scope.checkArtists = false, $scope.checkTracks = false;
+    $scope.opAlbums = false, $scope.opArtists = false, $scope.opTracks = false;
+    $scope.allAlbums = {},  $scope.allArtists = {},  $scope.allTracks = {};
+
+    $scope.search = function() {
+        types = '';
+        $scope.allAlbums = {},  $scope.allArtists = {},  $scope.allTracks = {};
+        $scope.opAlbums = false, $scope.opArtists = false, $scope.opTracks = false;
+
+        options();
+
+        if($scope.q !== '' && types !== '') {
+            $http({
+              method: 'GET',
+              url: '/songs.json?q=' + $scope.q + '&types=' + types + ''
+            }).then(function successCallback(response) {
+                if(response.data.songs.Song.albums.length > 0) {
+                    $scope.opAlbums = true;
+
+                    angular.forEach(response.data.songs.Song.albums, function(value, key) {
+                      $scope.allAlbums[key] = value;
+                    });
+                }
+                if(response.data.songs.Song.artists.length > 0) {
+                    $scope.opArtists = true;
+
+                    angular.forEach(response.data.songs.Song.artists, function(value, key) {
+                      $scope.allArtists[key] = value;
+                    });
+                }
+                if(response.data.songs.Song.tracks.length > 0) {
+                    $scope.opTracks = true;
+
+                    angular.forEach(response.data.songs.Song.tracks, function(value, key) {
+                      $scope.allTracks[key] = value;
+                    });
+                }
+
+                if($scope.opAlbums == false && $scope.opArtists == false && $scope.opTracks == false) {
+                    alert('No hay resultados para ' + $scope.q + '');
+                }
+                
+            }, function errorCallback(response) {
+                console.log(response);
+            });
+        }else {
+            alert('Debe ingresar termino de búsqueda y marcar una opción');
+        }
+    }
+
+    var options = function() {
+        if($scope.checkAlbums) {
+            types = types + '1';
+        }
+
+        if($scope.checkArtists) {
+            types = types + '2';
+        }
+
+        if($scope.checkTracks) {
+            types = types + '3';
+        }
+    }
 });
 
